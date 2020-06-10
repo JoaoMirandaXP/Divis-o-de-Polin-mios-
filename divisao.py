5168e88ad9002e72ab1bc3048d5aa77c15a9de31
RamON Coche <joaozinhocavalcante.jovc@gmail.com>
2020-06-10 13:32:39 -0300

    Bug fix

--- divisao.py
+++ divisao.py
@@ -4,10 +4,18 @@ coef_divisor = []
 coef_dividendo = []
 
 for x in range(o_dividendo+1):
-    coef_divisor.append(input("Insira o #{} coeficiente do dividendo : ".format(x+1))
+    coef_divisor.append(input("Insira o #{} coeficiente do dividendo : ".format(x+1)))
 
 for x in range(o_divisor+1):
-    coef_divisor.append(input("Insira o #{} coeficiente do divisor : ".format(x+1))  
+    coef_divisor.append(input("Insira o #{} coeficiente do divisor : ".format(x+1)))
 
 for x in coef_divisor:
     coef_dividendo[x] = x * coef_dividendo 
+
+#---------------------
+#---Resultado Final---
+#D(x) = 
+#d(x) = 
+#Q(x) =
+#R(x) =
+#---------------------
