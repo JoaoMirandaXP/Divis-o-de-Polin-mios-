3a67587026e85cad9bd24843d882a957f185c030
RamON Coche <joaozinhocavalcante.jovc@gmail.com>
2020-06-10 15:12:49 -0300

    tentativa de separação dos coeficientes

--- divisao.py
+++ divisao.py
@@ -13,14 +13,24 @@ for x in range(o_divisor+1):
 
 for x in range(o_dividendo+1):
     coef_dividendo.append(float(input("Insira o #{} coeficiente do dividendo : ".format(x+1))))
+for x in range(o_quociente+1):
+    coef_final.append([])
 
 #Distributiva da parte literal 
-for x in coef_divisor: 
-    
+for y in range(1,o_quociente+1):
+    for x in coef_divisor: 
+            coef_final[y].append(x*y)
+print(coef_final)
+def resolveSistema():
+    A = np.array(coef_final)
+    B = np.array(coef_dividendo) 
+    return np.linalg.inv(A).dot(B)
 
 print(coef_dividendo)
 print(coef_divisor)
 print(coef_final)
+#print(resolveSistema())
+
 #print(---------------------)
 #print(---Resultado Final---)
 #D(x) = 
