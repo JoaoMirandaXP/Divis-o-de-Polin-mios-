f39fcf21b26a582c907b67e5734b06a01b533487
RamON Coche <joaozinhocavalcante.jovc@gmail.com>
2020-06-10 13:14:05 -0300

    Primeiro commit

---
+++ divisao.py
@@ -0,0 +1,13 @@
+o_divisor = int(input("Ordem do divisor :"))
+o_dividendo = int(input("Ordem do dividendo :"))
+coef_divisor = []
+coef_dividendo = []
+
+for x in range(o_dividendo+1):
+    coef_divisor.append(input("Insira o #{} coeficiente do dividendo : ".format(x+1))
+
+for x in range(o_divisor+1):
+    coef_divisor.append(input("Insira o #{} coeficiente do divisor : ".format(x+1))  
+
+for x in coef_divisor:
+    coef_dividendo[x] = x * coef_dividendo 
