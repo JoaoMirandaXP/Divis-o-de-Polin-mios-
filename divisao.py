58b5647eaa402be71dc6246433099ecdd49b727a
RamON Coche <joaozinhocavalcante.jovc@gmail.com>
2020-06-11 18:12:24 -0300

    parte literal sem resto

--- divisao.py
+++ divisao.py
@@ -7,28 +7,49 @@ o_resto = o_divisor -1
 coef_divisor = []
 coef_dividendo = []
 coef_final = []
+sistema = []
 
 for x in range(o_divisor+1):
     coef_divisor.append(float(input("Insira o #{} coeficiente do divisor : ".format(x+1))))
 
 for x in range(o_dividendo+1):
     coef_dividendo.append(float(input("Insira o #{} coeficiente do dividendo : ".format(x+1))))
+    sistema.append([])
 for x in range(o_quociente+1):
     coef_final.append([])
 
 #Distributiva da parte literal 
-for y in range(1,o_quociente+1):
+for y in range(o_quociente+1):
     for x in coef_divisor: 
-            coef_final[y].append(x*y)
-print(coef_final)
+        coef_final[y].append(x)
+
+for x in range(o_dividendo+1):
+    for y in range(len(coef_dividendo)):
+        sistema[x].append(0) 
+print(sistema)
+
+def organizaSistema(coefs):
+    for x in range(len(sistema)):
+        if(x<len(coef_final)):
+            for y in range(len(coef_final)):
+                for z in coef_final[y]:
+                    sistema[x+y][x] = z
+    print(sistema)
+
+
+
 def resolveSistema():
-    A = np.array(coef_final)
+    a = np.array(sistema)
+    A = a.transpose()
     B = np.array(coef_dividendo) 
     return np.linalg.inv(A).dot(B)
 
 print(coef_dividendo)
 print(coef_divisor)
 print(coef_final)
+print(len(coef_final))
+print(len(sistema))
+organizaSistema(coef_final)
 #print(resolveSistema())
 
 #print(---------------------)
